2345513f9fd567988f3beeb338bbddbdb06e837d
Ruud op den Kelder <ruudodk@gmail.com>
2024-11-04 14:21:56 +0100

    build works

--- src/components/Globe.vue
+++ src/components/Globe.vue
@@ -256,7 +256,7 @@ function loadTexture(texturePath: string): THREE.Texture {
   )
 }
 
-const loadGlobeTexture = () => loadTexture('src/assets/earth.jpg')
+const loadGlobeTexture = () => loadTexture("/assets/earth.jpg')
 const loadSecondaryTexture = () => loadTexture('src/assets/earth_night.jpg')
 const loadCloudTexture = () => {
   const texture = loadTexture('src/assets/earth_clouds.jpg')
